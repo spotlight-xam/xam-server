97c194f1a17b3578acf5b5b276599270e1104842
51taek <wontaek88@gmail.com>
2023-03-16 16:08:48 +0900

    배포 하기 위해 error 내용 주석처리

--- src/main/java/spring/server/controller/PostController.java
+++ src/main/java/spring/server/controller/PostController.java
@@ -1,32 +1,32 @@
-package spring.server.controller;
-
-import static spring.server.result.ResultCode.*;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import spring.server.dto.feed.FeedPostResponse;
-import spring.server.result.ResultCode;
-import spring.server.result.ResultResponse;
-import spring.server.service.PostService;
-
-@RestController("post")
-@RequiredArgsConstructor
-@Slf4j
-public class PostController {
-
-    private final PostService postService;
-
-    @PostMapping
-    public ResponseEntity<FeedPostResponse> upload(@ModelAttribute FeedPostRequest feedPostRequest){
-        return ResponseEntity.ok().body(postService.upload(feedPostRequest));
-    }
-
-    @DeleteMapping("{postId}")
-    public ResponseEntity<ResultResponse> deleteFeed(@PathVariable Long feedId){
-        postService.delete(feedId);
-
-        return ResponseEntity.ok(ResultResponse.of(DeleteFeedSuccess));
-    }
-}
+//package spring.server.controller;
+//
+//import static spring.server.result.ResultCode.*;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import spring.server.dto.feed.FeedPostResponse;
+//import spring.server.result.ResultCode;
+//import spring.server.result.ResultResponse;
+//import spring.server.service.PostService;
+//
+//@RestController("post")
+//@RequiredArgsConstructor
+//@Slf4j
+//public class PostController {
+//
+//    private final PostService postService;
+//
+//    @PostMapping
+//    public ResponseEntity<FeedPostResponse> upload(@ModelAttribute FeedPostRequest feedPostRequest){
+//        return ResponseEntity.ok().body(postService.upload(feedPostRequest));
+//    }
+//
+//    @DeleteMapping("{postId}")
+//    public ResponseEntity<ResultResponse> deleteFeed(@PathVariable Long feedId){
+//        postService.delete(feedId);
+//
+//        return ResponseEntity.ok(ResultResponse.of(DeleteFeedSuccess));
+//    }
+//}
--- src/main/java/spring/server/service/PostService.java
+++ src/main/java/spring/server/service/PostService.java
@@ -1,54 +1,54 @@
-package spring.server.service;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import spring.server.controller.FeedPostRequest;
-import spring.server.dto.feed.FeedPostResponse;
-import spring.server.entity.Feed;
-import spring.server.entity.User;
-import spring.server.repository.FeedRepository;
-import spring.server.util.JwtUtil;
-
-import javax.transaction.Transactional;
-import java.util.Optional;
-
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class PostService {
-
-    private final JwtUtil jwtUtil;
-    private final FeedRepository feedRepository;
-
-    @Transactional
-    public FeedPostResponse upload(FeedPostRequest request){
-        User loginUser = jwtUtil.getLoginUser();
-
-        //이미지 업로드 추가 해야됨 (S3사용)
-        Feed feed = Feed.builder()
-                .title(request.getTitle())
-                .content(request.getContent())
-                .writer(loginUser)
-                .build();
-
-        feedRepository.save(feed);
-
-        return new FeedPostResponse(feed.getId());
-
-    }
-
-    @Transactional
-    public void delete(Long feedId){
-        Optional<Feed> feed = feedRepository.findById(feedId);
-        Long loginUserId = jwtUtil.getLoginUserId();
-
-//        if(loginUserId.equals(feed.get))
-
-    }
-
-    private Feed getFeed(Long feedId){
-        return feedRepository.findById(feedId)
-                .orElseThrow(() -> new RuntimeException());
-    }
-}
+//package spring.server.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//import spring.server.controller.FeedPostRequest;
+//import spring.server.dto.feed.FeedPostResponse;
+//import spring.server.entity.Feed;
+//import spring.server.entity.User;
+//import spring.server.repository.FeedRepository;
+//import spring.server.util.JwtUtil;
+//
+//import javax.transaction.Transactional;
+//import java.util.Optional;
+//
+//@Service
+//@Slf4j
+//@RequiredArgsConstructor
+//public class PostService {
+//
+//    private final JwtUtil jwtUtil;
+//    private final FeedRepository feedRepository;
+//
+//    @Transactional
+//    public FeedPostResponse upload(FeedPostRequest request){
+//        User loginUser = jwtUtil.getLoginUser();
+//
+//        //이미지 업로드 추가 해야됨 (S3사용)
+//        Feed feed = Feed.builder()
+//                .title(request.getTitle())
+//                .content(request.getContent())
+//                .writer(loginUser)
+//                .build();
+//
+//        feedRepository.save(feed);
+//
+//        return new FeedPostResponse(feed.getId());
+//
+//    }
+//
+//    @Transactional
+//    public void delete(Long feedId){
+//        Optional<Feed> feed = feedRepository.findById(feedId);
+//        Long loginUserId = jwtUtil.getLoginUserId();
+//
+////        if(loginUserId.equals(feed.get))
+//
+//    }
+//
+//    private Feed getFeed(Long feedId){
+//        return feedRepository.findById(feedId)
+//                .orElseThrow(() -> new RuntimeException());
+//    }
+//}
