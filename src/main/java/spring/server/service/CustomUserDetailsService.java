c2564f0d00827af863fb1e06b43c6ab0f043fd3e
taek <wontaek88@gmail.com>
2023-04-25 03:38:45 +0900

    CustomUserDetailsService 추가

---
+++ src/main/java/spring/server/service/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package spring.server.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import spring.server.entity.Member;
+import spring.server.repository.MemberRepository;
+import spring.server.result.error.exception.UserNotFoundException;
+
+import java.util.Collections;
+
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return memberRepository.findByUsername(username)
+                .map(this::createUserDetails)
+                .orElseThrow(UserNotFoundException::new);
+
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        // 나중에는 "USER" -> member.getRole().toString() 같은 방식으로 리팩토링 필요
+        final GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("USER");
+        return new User(
+                String.valueOf(member.getId()),
+                member.getPassword(),
+                Collections.singleton(grantedAuthority));
+    }
+}
