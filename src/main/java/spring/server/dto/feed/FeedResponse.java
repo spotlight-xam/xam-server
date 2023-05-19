c1472b1667c0d524d69ede04b739f9571b283218
taek <wontaek88@gmail.com>
2023-05-19 16:28:31 +0900

    FeedResponse 기능 최신화

--- src/main/java/spring/server/dto/feed/FeedResponse.java
+++ src/main/java/spring/server/dto/feed/FeedResponse.java
@@ -1,7 +1,12 @@
 package spring.server.dto.feed;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
+import spring.server.dto.image.ImageResponse;
+import spring.server.dto.image.QImageResponse;
 import spring.server.entity.Image;
 
 import java.util.ArrayList;
@@ -9,17 +14,13 @@ import java.util.List;
 
 @Data
 public class FeedResponse {
-
     private String title;
-
     private String writer;
-
-    private List<Image> images = new ArrayList<>();
-
+    private List<ImageResponse> images;
     private String content;
 
     @Builder
-    public FeedResponse(String title, String writer, List<Image> images, String content){
+    public FeedResponse(String title, String writer, List<ImageResponse> images, String content){
         this.title = title;
         this.writer = writer;
         this.images = images;
